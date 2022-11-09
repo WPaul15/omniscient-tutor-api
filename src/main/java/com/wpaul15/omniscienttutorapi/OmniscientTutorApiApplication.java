c7476aa7a0638b8c9358ab62e7dedfadc860b567
WPaul15 <willem.m.paul@gmail.com>
2022-11-08 21:04:06 -0600

    Initial commit

---
+++ src/main/java/com/wpaul15/omniscienttutorapi/OmniscientTutorApiApplication.java
@@ -0,0 +1,12 @@
+package com.wpaul15.omniscienttutorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniscientTutorApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OmniscientTutorApiApplication.class, args);
+  }
+}
---
+++ src/test/java/com/wpaul15/omniscienttutorapi/OmniscientTutorApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.wpaul15.omniscienttutorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniscientTutorApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
